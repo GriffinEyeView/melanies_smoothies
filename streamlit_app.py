1b944d0100eed8c1cb57d5647516bc481181b880
Derrick Griffin <derrickgriffin148@gmail.com>
2024-03-19 18:11:48 -0500

    Update streamlit_app.py
    fix: spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ if ingredients_list:
 # New section to display fruitvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruitvice_response.json(), use_container_width=True)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
